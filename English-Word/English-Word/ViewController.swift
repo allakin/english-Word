830bf9763fb717e02fea42b342b9918873510eb4
Pavel <19paul91@mail.ru>
2016-05-25 18:34:57 +0300

    add random Int

--- English-Word/English-Word/ViewController.swift
+++ English-Word/English-Word/ViewController.swift
@@ -10,6 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  var englishWord = ["Apartment", "Actor", "Arm", "Anger", "Ash", "Army", "Action", "Attitude", "Aisle", "After"]
+  var russianWord = ["Комната; Квартира", "Актер, Артист", "Рука", "Гнев", "Зола, Пепел; Прах", "Армия", "Поступок, Действие", "Позиция; Отношение к", "Проход", "prep- После, Через; adv- Потом"]
+  
+  
+  var english: String {
+    get {
+      return englishInput.text!
+    }
+    set{
+      let englishvalue = "\(newValue)"
+      englishInput.text = "\(newValue)"
+    }
+  }
+  
+  var russia: String {
+    get {
+      return russianInput.text!
+    }
+    set{
+      let russiavalue = "\(newValue)"
+      russianInput.text = "\(newValue)"
+    }
+  }
+
+  
+  @IBOutlet weak var englishInput: UILabel!
+  @IBOutlet weak var russianInput: UILabel!
+  @IBAction func buttonMore(sender: UIButton) {
+    
+    englishInput.text = String(arc4random_uniform(UInt32(englishWord.count)))
+    russianInput.text = russianWord[3]
+    
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
