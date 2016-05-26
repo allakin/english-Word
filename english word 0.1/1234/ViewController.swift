a3c0cb1eced800a546235354d6520ad983f3db71
Pavel <19paul91@mail.ru>
2016-05-26 22:07:16 +0300

    edit code

--- english word 0.1/1234/ViewController.swift
+++ english word 0.1/1234/ViewController.swift
@@ -9,7 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  var words = []
+  
+  
+  var english: String {
+    get {
+      return englishInput.text!
+    }
+    set{
+      let wordValue = "\(newValue)"
+      englishInput.text = "\(newValue)"
+    }
+  }
+  
+  @IBOutlet weak var englishInput: UILabel!
 
+  @IBAction func nextButton(sender: UIButton) {
+    
+    let randomIndex = Int(arc4random_uniform(UInt32(words.count)))
+    englishInput.text = [randomIndex]
+    
+  }
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
